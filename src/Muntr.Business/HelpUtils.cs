ee6f49fa6ba1fad0aaf1b0aa269b640c893540aa
Tobias Munter <tobbe@Tobiass-MacBook-Pro.local>
2016-08-19 14:52:17 +0200

    ...

---
+++ src/Muntr.Business/HelpUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Muntr.Business
+{
+    public static class HelpUtils
+    {
+        public static void Method1()
+        {    
+            Console.WriteLine("Hi form Business.");
+        }
+    }
+}
---
+++ src/Muntr.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Muntr.Business;
+
+namespace Muntr.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HelpUtils.Method1();
+        }
+    }
+}
